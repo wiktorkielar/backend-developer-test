86cf7747da131ce331671b32837fd6b354804372
Wiktor Kielar <wiktorkielar@gmail.com>
2021-10-17 19:15:08 +0200

    Project initialization

---
+++ src/main/java/com/wiktorkielar/backenddevelopertest/BackendDeveloperTestApplication.java
@@ -0,0 +1,13 @@
+package com.wiktorkielar.backenddevelopertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDeveloperTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDeveloperTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wiktorkielar/backenddevelopertest/BackendDeveloperTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiktorkielar.backenddevelopertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDeveloperTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
